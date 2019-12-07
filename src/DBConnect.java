97b97d0bc60c2febeeb32878545833db8451305f
eceogretmen <57140620+eceogretmen@users.noreply.github.com>
2019-12-07 20:09:27 +0300

    Database Java

---
+++ src/DBConnect.java
@@ -0,0 +1,21 @@
+
+import java.sql.*;
+import javax.swing.*;
+
+public class DBConnect {
+   
+    Connection con=null;
+    
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/healthcare?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey", "root","");
+            return con;
+            
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null,ex);
+            return null;
+        }
+    }
+}  
+    
